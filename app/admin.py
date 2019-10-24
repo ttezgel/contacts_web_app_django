43ddda88eb433a4d0a0fff884a0574afdbc01cfe
Tim Tezgel <tugi@MacBook-Pro.home>
2019-10-23 23:34:00 -0400

    first commit

---
+++ app/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Contact
+from import_export.admin import ImportExportActionModelAdmin
+
+
+class ContactAdmin(ImportExportActionModelAdmin):
+    list_display = ("id", "name", "gender", "email", "info", "phone", "date_added")
+    list_display_links = ('id', 'name')
+    list_editable = ("info",)
+    list_per_page = 10
+    search_fields = ("name", "gender", "email", "info", "phone")
+    list_filter = ("gender", "date_added")
+
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.unregister(Group)
